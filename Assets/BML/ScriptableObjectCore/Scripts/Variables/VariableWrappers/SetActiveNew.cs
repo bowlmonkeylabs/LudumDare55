3fcecda1ae3aef994d771d2d6b2bbfe1ec9c5941
Max Lepkowski <mlepkows@stevens.edu>
2024-04-15 20:54:42 -0400

    Show timer only in game scene.

---
+++ Assets/BML/ScriptableObjectCore/Scripts/Variables/VariableWrappers/SetActiveNew.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using BML.ScriptableObjectCore.Scripts.Variables;
+using BML.ScriptableObjectCore.Scripts.Variables.SafeValueReferences;
+using Sirenix.Utilities;
+using UnityEngine;
+
+namespace BML.ScriptableObjectCore.Scripts.Variables.VariableWrappers
+{
+    public class SetActiveNew : MonoBehaviour
+    {
+        [SerializeField] private SafeBoolValueReference Enabled;
+        [SerializeField] private GameObject[] Targets;
+
+        private void Start()
+        {
+            Targets.ForEach(t => t.SetActive(Enabled.Value));
+            Enabled.Subscribe(() =>
+            {
+                Targets.ForEach(t => t.SetActive(Enabled.Value));
+            });
+        }
+    }
+}
